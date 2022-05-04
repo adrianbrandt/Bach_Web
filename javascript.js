f3795e9f7b5382274ad568e61848cccdf49def2d
adrian <adrianb@hiof.no>
2022-05-04 21:36:00 +0200

    bugfixing

--- javascript.js
+++ javascript.js
@@ -29,4 +29,37 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('counter-display').innerHTML =  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
+    document.getElementById("sin").style.display = "none";
+    document.getElementById("sout").style.display = "block";
+
   };
+
+  function stampout(){
+
+    let content = document.getElementById('counter-display').innerHTML;
+    let clock = document.getElementById('clock').innerHTML;
+
+
+    var a = content.split(":");
+    var b = clock.split(":");
+
+    let h = b[0] - a[0];
+    let m = b[1] - a[1];
+    let s = b[2] - a[2];
+
+    console.log(h + ":" + m + ":" + s)
+
+
+    document.getElementById('counter-display').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById("scim").style.display = "block";
+    document.getElementById("sout").style.display = "none";
+  }
+
+  function stamped() {
+    let content = document.getElementById('counter-display').innerHTML;
+
+    if (confirm("successfully submitted time - " +content)) {
+      location.reload();
+    }
+  }
+
