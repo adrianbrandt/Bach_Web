197b8d3e390fd4ca993a7aaea81298f1aa55028e
adrian <adrianb@hiof.no>
2022-05-04 23:41:21 +0200

    added folder structure + bug fixes(i know it is alot now)

--- javascript.js
+++ javascript.js
@@ -11,12 +11,12 @@ function startTime() {
     let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
   };
   
   function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) {i = "0" + i};
     return i;
   };
 
@@ -27,7 +27,7 @@ function startTime() {
     let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('counter-display').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('counter-display').innerHTML ="Stemplet inn klokken: " +  h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
     document.getElementById("sin").style.display = "none";
     document.getElementById("sout").style.display = "block";
@@ -43,14 +43,13 @@ function startTime() {
     var a = content.split(":");
     var b = clock.split(":");
 
-    let h = b[0] - a[0];
-    let m = b[1] - a[1];
-    let s = b[2] - a[2];
-
+    let h = b[0] - a[1];
+    let m = b[1] - a[2];
+    let s = b[2] - a[3];
     console.log(h + ":" + m + ":" + s)
 
 
-    document.getElementById('counter-display').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('counter-display').innerHTML ="Total arbeids tid idag: " +  h + ":" + m + ":" + s;
     document.getElementById("scim").style.display = "block";
     document.getElementById("sout").style.display = "none";
   }
@@ -58,7 +57,7 @@ function startTime() {
   function stamped() {
     let content = document.getElementById('counter-display').innerHTML;
 
-    if (confirm("successfully submitted time - " +content)) {
+    if (confirm(content)) {
       location.reload();
     }
   }
